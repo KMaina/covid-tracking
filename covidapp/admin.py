44522c2f19c67a0365dfee39eed26f4f92bbedd6
benowino7 <bensonowino7@gmail.com>
2021-08-09 18:05:19 +0300

    registered site to admin

--- covidapp/admin.py
+++ covidapp/admin.py
@@ -1,5 +1,10 @@
 # type:ignore
 from django.contrib import admin
-from .models import Profile
+from .models import Profile,Patient,PatientInput,Doctor,DoctorsInput,ContactTracing,User
 
 admin.site.register(Profile)
+admin.site.register(PatientInput)
+admin.site.register(Patient)
+admin.site.register(DoctorsInput)
+admin.site.register(Doctor)
+admin.site.register(ContactTracing)
